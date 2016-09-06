5a25eb3d8da5bd3f1d020bdd2571fbf3bfc99d78
Dmitriy Vasiliev <dmitriyrv@gmail.com>
2016-09-06 15:36:22 +0300

    added controller

---
+++ src/main/java/com/helper/finance/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.helper.finance.controller;
+
+/**
+ * Created by dvas on 06.09.2016.
+ */
+public class UserController {
+}
--- src/main/java/com/helper/finance/user/bo/impl/UserBoImpl.java
+++ src/main/java/com/helper/finance/user/bo/impl/UserBoImpl.java
@@ -3,17 +3,21 @@ package com.helper.finance.user.bo.impl;
 import com.helper.finance.user.bo.UserBo;
 import com.helper.finance.user.dao.UserDao;
 import com.helper.finance.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by dvas on 06.09.2016.
  */
+@Service
 public class UserBoImpl implements UserBo {
 
+    @Autowired
     UserDao userDao;
 
-    public void setUserDao(UserDao userDao) {
+    /*public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
-    }
+    }*/
 
     @Override
     public void save(User user) {
