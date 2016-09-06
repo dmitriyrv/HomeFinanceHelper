d2166386c9b0e0335e653159759c3a046c8d4fa4
Dmitriy Vasiliev <dmitriyrv@gmail.com>
2016-09-06 14:49:19 +0300

    created model for User

---
+++ src/main/java/com/helper/finance/user/model/User.java
@@ -0,0 +1,63 @@
+package com.helper.finance.user.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by dvas on 06.09.2016.
+ */
+public class User implements Serializable {
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private boolean active;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
